58169a8c30ceffd12be28fb6633705bbd0973713
Beck Maldin <beckprograms@gmail.com>
2025-04-16 12:17:25 -0700

    auth added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Logout from "@/component/Logout";
 
 export default function Home() {
   return (
@@ -19,7 +20,7 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-
+        <Logout />
         <div className={styles.ctas}>
           <a
             className={styles.primary}
